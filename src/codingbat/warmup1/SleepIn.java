7bef3f2848e08ae7781f00fef905d5087aec4bc1
Omar Abdelaziz Hussein <74055004+omarHussein3@users.noreply.github.com>
2023-09-28 19:59:17 +0200

    Solving problem on CodingBat

---
+++ src/codingbat/warmup1/SleepIn.java
@@ -0,0 +1,15 @@
+package codingbat.warmup1;
+
+public class SleepIn {
+
+    public static boolean sleepIn(boolean weekday, boolean vacation) {
+        return weekday == false || vacation == true;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sleepIn(false, false));
+        System.out.println(sleepIn(true, false));
+        System.out.println(sleepIn(false, true));
+    }
+}
