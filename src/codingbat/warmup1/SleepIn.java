a5b453c2702f1f72b599032a4827a8160ca00fef
omarHussein3 <20192801@fue.edu.eg>
2023-12-17 22:39:02 +0200

    lkjn

--- src/codingbat/warmup1/SleepIn.java
+++ src/codingbat/warmup1/SleepIn.java
@@ -5,6 +5,7 @@ public class SleepIn {
     public static boolean sleepIn(boolean weekday, boolean vacation) {
         return weekday == false || vacation == true;
 
+
     }
 
     public static void main(String[] args) {
