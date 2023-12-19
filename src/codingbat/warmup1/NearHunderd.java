469aaa9c14e2107632bafbcc09ee62e115b01aa8
omarHussein3 <20192801@fue.edu.eg>
2023-12-19 20:04:46 +0200

    solving codingbat problems

---
+++ src/codingbat/warmup1/NearHunderd.java
@@ -0,0 +1,15 @@
+package codingbat.warmup1;
+
+public class NearHunderd
+{
+
+    public static boolean nearHundred(int n) {
+        return ((Math.abs(100 - n) <= 10) || (Math.abs(200 - n) <= 10));
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(nearHundred(93));
+        System.out.println(nearHundred(90));
+        System.out.println(nearHundred(89));
+    }
+}
