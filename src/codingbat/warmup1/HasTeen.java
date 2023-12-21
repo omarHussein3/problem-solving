ac19e40607c805c3982ecca75b713de4305567f1
omarHussein3 <20192801@fue.edu.eg>
2023-12-21 02:41:17 +0200

    solving codingbat problems

---
+++ src/codingbat/warmup1/HasTeen.java
@@ -0,0 +1,15 @@
+package codingbat.warmup1;
+
+public class HasTeen
+{
+    public static boolean hasTeen(int a, int b, int c) {
+
+        return (a>=13 && a<=19) || (b>=13 && b<=19) || (c>=13 && c<=19);
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(hasTeen(13, 20, 10));
+        System.out.println(hasTeen(20, 19, 10));
+        System.out.println(hasTeen(20, 10, 13));
+    }
+}
