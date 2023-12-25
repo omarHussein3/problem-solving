5d05c8e770c5a2241ec62565e625aba0c347b393
omarHussein3 <20192801@fue.edu.eg>
2023-12-25 04:08:11 +0200

    solving codingbat problems

---
+++ src/codingbat/string1/MakeoutWord.java
@@ -0,0 +1,14 @@
+package codingbat.string1;
+
+public class MakeoutWord {
+    public static String makeOutWord(String out, String word) {
+        return out.substring(0, 2) + word + out.substring(2, 4);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(makeOutWord("<<>>", "Yay"));
+        System.out.println(makeOutWord("<<>>", "WooHoo"));
+        System.out.println(makeOutWord("[[]]", "word"));
+    }
+}
+
