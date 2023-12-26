bb7eb1f3b1905a7428e11cd89eb3ee911917494f
omarHussein3 <20192801@fue.edu.eg>
2023-12-26 23:06:12 +0200

    solving codingbat problems

---
+++ src/codingbat/string1/WithoutX.java
@@ -0,0 +1,29 @@
+package codingbat.string1;
+
+public class WithoutX
+{
+    public static String withoutX(String str) {
+        if (str.length() == 0)
+            return str;
+        if (str.length() == 1){
+            if (str.charAt(0) == 'x')
+                return "";
+            else
+                return str;
+        }
+        if (str.charAt(0) == 'x')
+            str = str.substring(1,str.length());
+
+        if (str.charAt(str.length()-1) == 'x')
+            str = str.substring(0,str.length()-1);
+
+        return str;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(withoutX("hi"));
+        System.out.println(withoutX("xip"));
+        System.out.println(withoutX("xhi"));
+
+    }
+}
