e73493cd258867e712d53ba99b5498f0daf44d3f
omarHussein3 <20192801@fue.edu.eg>
2023-12-26 21:07:06 +0200

    solving codingbat problems

---
+++ src/codingbat/string1/TheEnd.java
@@ -0,0 +1,18 @@
+package codingbat.string1;
+
+public class TheEnd
+{
+    public static String theEnd(String str, boolean front) {
+        if (front)
+        return str.substring(0,1);
+  else
+        return str.substring(str.length()-1);
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(theEnd("Hello", true));
+        System.out.println(theEnd("Hello", false));
+        System.out.println(theEnd("oh", false));
+
+    }
+}
