8eda52a40bbcc6b1ff821637585382adcafad964
omarHussein3 <20192801@fue.edu.eg>
2023-12-27 04:31:17 +0200

    solving codingbat problems

---
+++ src/codingbat/warmup2/Last2.java
@@ -0,0 +1,27 @@
+package codingbat.warmup2;
+
+public class Last2
+{
+    public static int last2(String str) {
+        if (str.length() < 2) return 0;
+
+        String end = str.substring(str.length()-2);
+        int count = 0;
+
+        for (int i=0; i<str.length()-2; i++) {
+            String sub = str.substring(i, i+2);
+            if (sub.equals(end)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(last2("hixxhi"));
+        System.out.println(last2("xaxxaxaxx"));
+        System.out.println(last2("axxxaaxx"));
+
+    }
+}
