e96f53ca188b53f73de424fe995fee7ac7f4eade
omarHussein3 <20192801@fue.edu.eg>
2024-01-09 20:11:11 +0200

    solving codingbat problems

--- src/codingbat/warmup2/Arrayfront9.java
+++ src/codingbat/warmup2/Arrayfront9.java
@@ -13,12 +13,12 @@ public class Arrayfront9
 
         return false;
     }
-    }
     public static void main(String[] args) {
-        System.out.println(arrayFront9();
-        System.out.println(arrayFront9();
-        System.out.println(arrayFront9();
+            System.out.println(arrayFront9(new int[]{1, 2, 9, 3, 4}));
+            System.out.println(arrayFront9(new int[]{1, 2, 3, 4, 9}));
+            System.out.println(arrayFront9(new int[]{1, 2, 3, 4, 5}));
+
+        }
 
     }
 
-}
