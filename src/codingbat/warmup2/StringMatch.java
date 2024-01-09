e32e71a82aedda7ea00a413beae98e7eea9cc8ff
omarHussein3 <20192801@fue.edu.eg>
2024-01-09 20:59:36 +0200

    solving codingbat problems

--- src/codingbat/warmup2/StringMatch.java
+++ src/codingbat/warmup2/StringMatch.java
@@ -3,17 +3,17 @@ package codingbat.warmup2;
 public class StringMatch
 {
     public static int stringMatch(String a, String b) {
-        int len = Math.min(a.length(), b.length());
         int count = 0;
+        int L = Math.min(a.length(), b.length());
 
-        for (int i=0; i<len-1; i++) {
-            String aa = a.substring(i, i+2);
-            String bb = b.substring(i, i+2);
-            if (aa.equals(bb)) {
+
+        for (int i=0; i<L-1; i++) {
+            String A = a.substring(i, i+2);
+            String B = b.substring(i, i+2);
+            if (A.equals(B)) {
                 count++;
             }
         }
-
         return count;
     }
     public static void main(String[] args) {
